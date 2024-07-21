fc6006487a5a766e9c9294a881111269cd026df2
pang <eys555@naver.com>
2024-07-21 23:07:58 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.14 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int result = 0;
+        result = (num1 * 1000)/ num2 ;
+        return result;
+    }
+}
