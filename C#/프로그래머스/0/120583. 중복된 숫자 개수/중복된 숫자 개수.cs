408d3f5ddc797b9fa777bae089acc9c1d7aacae4
pang <eys555@naver.com>
2024-08-05 17:30:58 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.27 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int[] array, int n) 
+    {
+        int count = 0;
+        for(int i = 0; i < array.Length; i++)
+        {
+            if(array[i] == n){
+                count += 1;
+            }
+        }
+        return count;
+    }
+}
