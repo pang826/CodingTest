35037b7ca087514e2ee70769828fcd17b4588453
pang <eys555@naver.com>
2025-08-01 00:10:22 +0900

    [level 0] Title: 7의 개수, Time: 0.18 ms, Memory: 31.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        for(int i = 0; i < array.Length; i++)
+        {
+            int a = array[i];
+            while(a > 0)
+            {
+                if(a  % 10 == 7)
+                    answer++;
+                a /= 10;
+            }
+        }
+        return answer;
+    }
+}
