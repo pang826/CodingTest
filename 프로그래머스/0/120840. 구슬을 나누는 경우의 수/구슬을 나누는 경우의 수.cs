c225061019f833929da6c2689698c3308d8693b8
pang <eys555@naver.com>
2025-08-02 19:35:38 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.26 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public long solution(int balls, int share) {
+        long answer = 1;
+        int shareIndex = 1;
+        for(int i = share+1; i <= balls; i++){
+            answer *= i;
+            answer /= shareIndex;
+            shareIndex++;
+        }
+        
+        return (int)answer;
+    }
+}
