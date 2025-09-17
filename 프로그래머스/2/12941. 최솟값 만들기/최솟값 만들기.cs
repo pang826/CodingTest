dc4acbca70f22a96b85ed40653006f34c08a9589
pang <eys555@naver.com>
2025-09-17 11:41:35 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.21 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+public class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Array.Sort(A);
+        Array.Sort(B);
+        Array.Reverse(B);
+        
+        for(int i = 0; i < A.Length; i++)
+        {
+            answer += A[i] * B[i];
+        }
+        return answer;
+    }
+}
